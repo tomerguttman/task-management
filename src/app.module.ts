bf881684175cccbb211b7885f446946d4aa1e0eb
tomerguttman <tomer.guttman@payu.com>
2025-08-03 16:04:27 +0300

    feat: add .env validation support

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AuthModule } from './auth/auth.module';
 import { LoggerModule } from 'nestjs-pino';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ENV } from './helpers/constants';
+import { configSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`], // Load environment variables from .env file
+      validationSchema: configSchema,
       isGlobal: true,
     }),
     TasksModule,
---
+++ src/config.schema.ts
@@ -0,0 +1,14 @@
+import * as Joi from '@hapi/joi';
+
+export const configSchema = Joi.object({
+  STAGE: Joi.string().valid('dev', 'prod').required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_HOST: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+  KAFKA_ADDRESS: Joi.string().required(),
+  KAFKA_CLIENT_ID: Joi.string().required(),
+  KAFKA_GROUP_ID: Joi.string().required(),
+  KAFKA_TASKS_TOPIC: Joi.string().required(),
+});
